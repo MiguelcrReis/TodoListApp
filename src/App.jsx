5c50902c635fe9879409598dd75a1a01ec925e2c
MiguelcrReis <miguelcr.reis@gmail.com>
2021-11-28 10:41:54 -0300

    authenticated correction

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
+import logo from './logo.svg';
+import './App.css';
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>We now have Auth!</h1>
+      </header>
+      <AmplifySignOut />
+    </div>
+  );
+}
+
+export default withAuthenticator(App);
